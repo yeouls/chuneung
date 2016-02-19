d5c12df25415331b732ee1f797faf2ec95acd2de
yeouls <fairily214@gmail.com>
2016-02-19 14:59:04 +0900

    yeoul
    기본 소스 셋팅

---
+++ src/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name chuneungApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the chuneungApp
+ */
+angular.module('chuneungApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
